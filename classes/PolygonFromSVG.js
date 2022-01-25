df33876bc38db75c142548027881cedc3fc5b5d1
ElijahLangenegger <elijah.langenegger@hfg-gmuend.de>
2022-01-25 10:39:00 +0100

    Update Murmelbahn

--- classes/PolygonFromSVG.js
+++ classes/PolygonFromSVG.js
@@ -1,9 +1,9 @@
 /*
 Usage:
 // SVG embedded in HTML path defined via elem attribute
-let block = new PolygonFromSVG(world, { x: 300, y: 500, fromId: 'puzzle', scale: 0.6, color: 'lime' }, { isStatic: true, friction: 0.0 });
+let block = new SVGBlock(world, { x: 300, y: 500, elem: 'puzzle', scale: 0.6, color: 'lime' }, { isStatic: true, friction: 0.0 });
 // SVG in separate file path defined via file attribute
-let block = new PolygonFromSVG(world, { x: 580, y: 710, fromFile: './path.svg', scale: 0.6, color: 'yellow' }, { isStatic: true, friction: 0.0 });
+let block = new SVGBlock(world, { x: 580, y: 710, file: './path.svg', scale: 0.6, color: 'yellow' }, { isStatic: true, friction: 0.0 });
 */
 class PolygonFromSVG extends Block {
   constructor(world, attrs, options) {
@@ -11,24 +11,54 @@ class PolygonFromSVG extends Block {
   }
 
   addBody() {
-    if (this.attrs.fromId) {
-      // find SVG embedded in current HTML document
-      const $element = document.getElementById(this.attrs.fromId);
-      if ($element) {
-        this.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(Matter.Svg.pathToVertices($element, 10), this.attrs.scale, this.attrs.scale), this.options);
-        Matter.Body.setPosition(this.body, this.attrs);
-      }
+    let vertices;
+    if (this.attrs.path) {
+        // use a path provided directly
+        vertices = Matter.Svg.pathToVertices(this.attrs.path, 10);
+        this.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(vertices, this.attrs.scale, this.attrs.scale), this.options);
+        Matter.Body.setPosition(this.body, this.getCenter(vertices));
     } else {
-      // load external SVG file
-      let that = this;
-      httpGet(this.attrs.fromFile, "text", false, function(response) {
-        const parser = new DOMParser();
-        const svgDoc = parser.parseFromString(response, "image/svg+xml");
-        const path = svgDoc.querySelector("path");
-        that.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(Matter.Svg.pathToVertices(path, 10), that.attrs.scale, that.attrs.scale), that.options);
-        Matter.Body.setPosition(that.body, that.attrs);
-        Matter.World.add(that.world, that.body);
-      });
+      if (this.attrs.elem) {
+        // use a path of SVG embedded in current HTML page
+        let path = document.getElementById(this.attrs.elem);
+        if (null != path) {
+          vertices = Matter.Svg.pathToVertices(path, 10);
+          this.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(vertices, this.attrs.scale, this.attrs.scale), this.options);
+          if (this.attrs.x) {
+            Matter.Body.setPosition(this.body, this.attrs);
+          } else {
+            Matter.Body.setPosition(this.body, this.getCenter(vertices));
+          }
+        }
+      } else {
+        // use a path in separate SVG file
+        let that = this;
+        httpGet(this.attrs.file, "text", false, function(response) {
+          const parser = new DOMParser();
+          const svgDoc = parser.parseFromString(response, "image/svg+xml");
+          const path = svgDoc.querySelector("path");
+          vertices = Matter.Svg.pathToVertices(path, 10);
+          that.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(vertices, that.attrs.scale, that.attrs.scale), that.options);
+          if (this.attrs.x) {
+            Matter.Body.setPosition(this.body, this.attrs);
+          } else {
+            Matter.Body.setPosition(this.body, this.getCenter(vertices));
+          }
+          Matter.World.add(engine.world, [that.body]);
+        });
+      }
     }
+  }
+
+  getCenter(vertices) {
+    let min = {x: 999999, y: 999999};
+    let max = {x: -999999, y: -999999};
+    vertices.forEach((v, i) => {
+      min.x = min.x > v.x ? v.x : min.x;
+      min.y = min.y > v.y ? v.y : min.y;
+      max.x = max.x < v.x ? v.x : max.x;
+      max.y = max.y < v.y ? v.y : max.y;
+    });
+    return { x: min.x + (this.body.position.x - this.body.bounds.min.x), y: min.y + (this.body.position.y - this.body.bounds.min.y) }
   }
 }
--- selina-svg-many-rects/sketch.js
+++ selina-svg-many-rects/sketch.js
@@ -4,76 +4,145 @@ let kopfImg;
 let ball;
 let box;
 let mouse;
-//let hitSound;
+let hitSound;
+let audio;
+let engine = Matter.Engine.create();
+let world = engine.world;
 
-// function preload() {
-//   // load sound
-//   hitSound = loadSound('sound.mp3');
-//   hitSound.playMode('sustain');
-// }
+function preload() {
+  //load sound
+  soundFormats('mp3', 'ogg');
+  hitSound = loadSound('sound.mp3');
+  //hitSound.playMode('sustain');
+
+  new BlocksFromSVG(world, "svgRects2.svg", blocks, {
+    isStatic: true
+  });
+}
 
 function setup() {
-  const canvas =createCanvas(2455, 600);
-  let engine = Matter.Engine.create();
-  let world = engine.world;
+  const canvas = createCanvas(2455, 600);
+  // let engine = Matter.Engine.create();
+  // let world = engine.world;
 
-  svgRects = document.getElementsByTagName('rect');
-  for (let r=0; r<svgRects.length;r++) {
-    let w = +svgRects[r].getAttribute('width')
-    let h = +svgRects[r].getAttribute('height')
-    blocks.push(new BlockCore(
-      world,
-      {
-        x: +svgRects[r].getAttribute('x') + w / 2,
-        y: +svgRects[r].getAttribute('y') + h / 2,
-        w: w,
-        h: h,
-        color: 'blue',
-      },
-      { isStatic: true}
-    ));
-  }
+  // new BlocksFromSVG(world, "svgRects2.svg", blocks, {
+  //   isStatic: true
+  // });
+
+  //Blocks[19] soll label 'knopf' bekommen
+    blocks[19].body.label= 'knopf';
+
+  //Blocks[18] soll höhere Restitution bekommen
+    blocks[18].body.restitution = 8;
+
+
+  // svgRects = document.getElementsByTagName('rect');
+  // for (let r=0; r<svgRects.length;r++) {
+  //   let w = +svgRects[r].getAttribute('width');
+  //   let h = +svgRects[r].getAttribute('height');
+
+  // let rectAngle = svgRects[r].getAttribute('transform')
+  // let angleString;
+  // if(rectAngle != null) {
+  //   angleString = rectAngle.substring(7,13)
+  // } else {
+  //   anglString = 0
+  // }
+
+  //if svgRects[bestimmtes r] dann soll angle (per hand eingegebener wert) sein
+
+  // console.log(svgRects[r]);
+  // console.log(r);
+  // console.log(angleString);
+  // console.log(typeof(rectAngle));
+
+  // blocks.push(new BlockCore(
+  //   world,
+  //   {
+  //     x: +svgRects[r].getAttribute('x') + w / 2,
+  //     y: +svgRects[r].getAttribute('y') + h / 2,
+  //     w: w,
+  //     h: h,
+  //     color: 'blue'
+  //   },
+  //   { isStatic: true
+  //     angle: angleString }
+  // ));
 
   //load images
   kopfImg = loadImage('kopf.png');
 
   //add bodies
-  box = new Block(world, { x: 150, y: 200, w: 40, h: 20, color: 'grey' }, {isStatic: true, restitution:8, label:'knopf'});
-  ball = new SpriteBall(world, { x: 100, y: 50, r: 10, image: kopfImg}, {label:'ball'});
-  ground = new Block(world, { x: 400, y: 430, w: 810, h: 15, color: 'grey' }, { isStatic: true });
+  box = new Block(world, {
+    x: 150,
+    y: 200,
+    w: 40,
+    h: 20,
+    color: 'grey'
+  }, {
+    isStatic: true,
+    restitution: 0.8,
+    label: 'knopf'
+  });
+
+  ball = new SpriteBall(world, {
+    x: 100,
+    y: 50,
+    r: 10,
+    image: kopfImg
+  }, {
+    label: 'ball'
+  });
+
+  ground = new Block(world, {
+    x: 400,
+    y: 430,
+    w: 810,
+    h: 15,
+    color: 'grey'
+  }, {
+    isStatic: true
+  });
 
   //setup mouse
   mouse = new Mouse(engine, canvas);
 
   //sound
-  // Matter.Events.on(engine, 'collisionStart', function(event) {
-  //   const pairs = event.pairs[0];
-  //   const bodyA = pairs.bodyA;
-  //   const bodyB = pairs.bodyB;
-  //   if (bodyA.label === "ball" || bodyB.label === "knopf") {
-  //     hitSound.play();
-  //   }
-  // });
+  Matter.Events.on(engine, 'collisionStart', function(event) {
+    //console.log("erster test");
+    const pairs = event.pairs[0];
+    const bodyA = pairs.bodyA;
+    const bodyB = pairs.bodyB;
+    console.log(bodyA.label, bodyB.label);
+    if ((bodyA.label === "ball" && bodyB.label === "knopf") || (bodyA.label === "knopf" && bodyB.label === "ball")) {
+      hitSound.play();
+    }
+  });
 
   // run the engine
   Matter.Runner.run(engine);
 }
 
 //jump
-function keyPressed(e){
-  if (keyCode == "32"){
+function keyPressed(e) {
+  if (keyCode == "32") {
     Matter.Body.applyForce(
-      ball.body,
-      {x: ball.body.position.x, y: ball.body.position.y},
-      {x: 0.002, y: -0.008 }
+      ball.body, {
+        x: ball.body.position.x,
+        y: ball.body.position.y
+      }, {
+        x: 0.002,
+        y: -0.008
+      }
     );
   }
   // prevent accidentally scrolling of website with SPACE key
-  if(e.keyCode == 32 && e.target == document.body) {
+  if (e.keyCode == 32 && e.target == document.body) {
     e.preventDefault();
   }
 }
 
+//Zeichnet alle Objekte
 function draw() {
   clear();
   ground.draw();
@@ -81,6 +150,6 @@ function draw() {
   box.draw();
   mouse.draw();
   for (let b of blocks) {
-    b.draw();
+     b.draw();
   }
 }
